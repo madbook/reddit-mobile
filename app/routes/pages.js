e37423bc8e8d068471ca4093393cafda858a11e5
Jack Lawson <ajacksified@gmail.com>
2014-09-10 16:31:32 -0700

    Really simple caching

--- app/routes/pages.js
+++ app/routes/pages.js
@@ -1,7 +1,42 @@
 var React = require('react');
 var _ = require('lodash');
+var q = require('q');
+var querystring = require('querystring');
 
 var V1Api = require('snoode').v1;
+
+// TODO: NOTE: NOT PROD READY, MEMORY, WILL DIE
+var viewCache = {};
+
+function cache(template, seconds, app, req, res, fn) {
+  var defer = q.defer();
+
+  var cacheKey = req.url;
+
+  res.setHeader('Cache-Control', 'public, max-age=' + seconds);
+  var now = new Date();
+
+  if (viewCache[cacheKey]) {
+    if(now < viewCache[cacheKey].nextRender) {
+      defer.resolve({ res: res, page: viewCache[cacheKey].renderedPage});
+    }
+  } else {
+    viewCache[cacheKey] = {};
+    viewCache[cacheKey] = viewCache[cacheKey];
+  }
+
+  fn(req).done(function(props) {
+    app.render(template, props, function(err, str) {
+      now = Date(now.setSeconds(now.getSeconds() + 10));
+      viewCache[cacheKey].nextRender = now;
+      viewCache[cacheKey].renderedPage = str;
+
+      res.send(str);
+    });
+  });
+
+  return defer.promise;
+}
 
 module.exports = function(app) {
   var reddit = new V1Api({
@@ -23,84 +58,102 @@ module.exports = function(app) {
   }
 
   app.get('/', function(req, res) {
-    var props = buildProps(req, { });
-    var options = {}
+    cache('pages/index', 30, app, req, res, function(req) {
+      var defer = q.defer();
 
-    if (req.query.count && req.query.count <= 25) {
-      options.count = req.query.count;
-    }
+      var props = buildProps(req, { });
+      var options = {}
 
-    if (req.query.after) {
-      options.after = req.query.after;
-    }
+      if (req.query.count && req.query.count <= 25) {
+        options.count = req.query.count;
+      }
 
-    props.page = req.query.page || 0;
+      if (req.query.after) {
+        options.after = req.query.after;
+      }
 
-    reddit.links().get(options).done(function(data){
-      props.listings = data;
-      res.render('pages/index', props);
+      props.page = req.query.page || 0;
+
+      reddit.links().get(options).done(function(data){
+        props.listings = data;
+        defer.resolve(props);
+      });
+
+      return defer.promise;
     });
   });
 
   app.get('/r/:subreddit', function(req, res) {
-    var props = buildProps(req, { });
+    cache('pages/index', 30, app, req, res, function(req) {
+      var defer = q.defer();
 
-    var options = {
-      subreddit: req.params.subreddit
-    }
+      var props = buildProps(req, { });
 
-    if (req.query.count && req.query.count <= 25) {
-      options.count = req.query.count;
-    }
+      var options = {
+        subredditName: req.params.subreddit
+      }
 
-    if (req.query.after) {
-      options.after = req.query.after;
-    }
+      if (req.query.count && req.query.count <= 25) {
+        options.count = req.query.count;
+      }
 
-    props.page = req.query.page || 0;
+      if (req.query.after) {
+        options.after = req.query.after;
+      }
 
-    reddit.links().get(options).done(function(data){
-      props.listings = data;
+      props.page = req.query.page || 0;
 
-      props.hideSubredditLabel = true;
+      reddit.links().get(options).done(function(data){
+        props.listings = data;
 
-      res.render('pages/index', props);
+        props.hideSubredditLabel = true;
+
+        defer.resolve(props);
+      });
+
+      return defer.promise;
     });
   });
 
   app.get('/r/:subreddit/comments/:listingId/:listingTitle', function(req, res) {
-    var props = buildProps(req, { });
+    cache('pages/listing', 5, app, req, res, function(req) {
+      var defer = q.defer();
 
-    function decodeHtmlEntities(html){
-      return html.replace(/&gt;/g, '>').replace(/&lt;/g, '<');
-    }
+      var props = buildProps(req, { });
 
-    function mapComment(comment) {
-      if (!comment.body) {
-        props.listing.more = comment;
-      } else {
-        comment.body_html = decodeHtmlEntities(comment.body_html);
-
-        if (comment.replies){
-          comment.replies = comment.replies.map(mapComment) || [];
-        } else {
-          comment.replies = [];
-        }
-
-        return comment;
+      function decodeHtmlEntities(html){
+        return html.replace(/&gt;/g, '>').replace(/&lt;/g, '<');
       }
-    }
 
-    reddit.comments().get({
-      linkId: req.params.listingId
-    }).done(function(data){
-      props.listing = data.listing;
+      function mapComment(comment) {
+        if (!comment.body) {
+          props.listing.more = comment;
+        } else {
+          comment.body_html = decodeHtmlEntities(comment.body_html);
 
-      props.comments = data.comments.map(function(comment){
-        return mapComment(comment, 0);
+          if (comment.replies){
+            comment.replies = comment.replies.map(mapComment) || [];
+          } else {
+            comment.replies = [];
+          }
+
+          return comment;
+        }
+      }
+
+      reddit.comments().get({
+        linkId: req.params.listingId
+      }).done(function(data){
+        props.listing = data.listing;
+
+        props.comments = data.comments.map(function(comment){
+          return mapComment(comment, 0);
+        });
+
+        defer.resolve(props);
       });
 
-      res.render('pages/listing', props);
+      return defer.promise;
     });
   });
 }
