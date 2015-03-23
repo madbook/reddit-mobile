c814d39baf97d32b0423efdd55204ec3912f1498
Jack Lawson <ajacksified@gmail.com>
2015-03-22 23:09:25 -0700

    Only show gilded icon once clicked through

--- src/views/components/Listing.jsx
+++ src/views/components/Listing.jsx
@@ -231,7 +231,7 @@ class Listing extends React.Component {
       );
     }
 
-    if (listing.gilded) {
+    if (listing.gilded && props.single) {
       gilded = (
         <li><GoldIcon/></li>
       );
