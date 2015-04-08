100e30d0c1187c3cad33dc8fde12aca9a8fee4f5
Dane Hansen <dane@danehansen.com>
2015-04-07 19:30:46 -0700

    Replace current loading image with svg animation

---
+++ src/views/components/RIcon.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import DrawSVGPlugin from '../../lib/greensock/plugins/DrawSVGPlugin.min.js';
+
+import SVGFactory from '../components/SVG';
+var SVG;
+
+
+
+class RIcon extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {outline: false};
+    this._play = this._play.bind(this);
+    this.tweenOn = this.tweenOn.bind(this);
+    this.tweenOff = this.tweenOff.bind(this);
+  }
+
+  render() {
+    return (
+      <SVG className='SVG-icon RIcon' width={20} height={20} fallbackText='comment'>
+        <circle className='SVG-fill' cx='10' cy='10' r='10'/>
+        <path ref='r' className={'SVG-fill-bg' + (this.state.outline?' outline':'')} d="M12.198584,5.704752c-1.356266,0-2.418676,0.421916-3.200488,1.089559 C8.646981,6.258162,8.041927,5.903345,7.35319,5.903345H7.333333v4.765951v3.177311v0.164551 c0,0.528857,0.399096,0.990633,0.926847,1.024789c0.578288,0.037427,1.058969-0.420573,1.058969-0.990764v-0.198576v-3.673763 c0-1.370922,1.111352-2.482275,2.482267-2.482275c0.822558,0.000008,1.489364-0.666805,1.489364-1.489356V5.704761 L12.198584,5.704752z"/>
+      </SVG>
+    );
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (!SVG.ENABLED) {
+      return;
+    }
+    var played = nextProps.played;
+    if (typeof played !== 'undefined' && played !== this.props.played) {
+      this._play(played);
+    }
+  }
+
+  _play(bool) {
+    //TODO
+  }
+
+  tweenOn(callback) {
+    this.setState({outline:true});
+    var timeline = new TimelineLite({onComplete:callback});
+    var r = this.refs.r.getDOMNode();
+    timeline.add(TweenLite.from(r, 1, {drawSVG:0, ease:Cubic.easeInOut}));
+
+    timeline.add(TweenLite.from(r, 0.5, {fill:'transparent'}));
+    timeline.add(TweenLite.to(r, 0.5, {strokeWidth:0, autoRound:false}), 1);
+  }
+
+  tweenOff(callback) {
+    var timeline = new TimelineLite({onComplete:callback});
+    var r = this.refs.r.getDOMNode();
+    timeline.add(TweenLite.to(r, 0.5, {fill:'transparent'}));
+    timeline.add(TweenLite.to(r, 0.5, {strokeWidth:0.5, autoRound:false}), 0);
+
+    timeline.add(TweenLite.to(r, 1, {drawSVG:0, ease:Cubic.easeInOut}));
+  }
+}
+
+RIcon.defaultProps = {
+  played: false,
+};
+
+function RIconFactory(app) {
+  SVG = SVGFactory(app);
+  return app.mutate('core/components/RIcon', RIcon);
+}
+
+export default RIconFactory;
---
+++ src/views/components/SnooIcon.jsx
@@ -0,0 +1,145 @@
+import React from 'react';
+import MyMath from '../../lib/danehansen/MyMath';
+import DrawSVGPlugin from '../../lib/greensock/plugins/DrawSVGPlugin.min.js';
+
+import SVGFactory from '../components/SVG';
+var SVG;
+
+
+class SnooIcon extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this._play = this._play.bind(this);
+    this.tweenOn = this.tweenOn.bind(this);
+    this.tweenOff = this.tweenOff.bind(this);
+    this._blink = this._blink.bind(this);
+    this._blinkTimeout = this._blinkTimeout.bind(this);
+    this.startBlinking = this.startBlinking.bind(this);
+    this._blinkable = false;
+    this._mounted = true;
+  }
+
+  render() {
+    return (
+      <SVG className='SVG-icon SnooIcon' width={20} height={20} fallbackText='comment'>
+        <circle className='SVG-fill' cx='10' cy='10' r='10'/>
+        <path ref='face' className='SVG-fill-bg' d='M15.333333,11.499898c0-2.113655-2.392639-3.833231-5.333565-3.833231 c-2.940686,0-5.333102,1.719576-5.333102,3.833231c0,2.113753,2.392416,3.833435,5.333102,3.833435 C12.940694,15.333333,15.333333,13.613651,15.333333,11.499898z'/>
+        <circle ref='dingleberry' className='SVG-fill-bg' cx='14.166199' cy='5' r='1'/>
+        <g fill='none' className='SVG-stroke-bg' strokeWidth='0.466667' strokeLinecap='round'>
+          <line ref='stem' x1='14.166667' y1='5.012208' x2='10.756104' y2='4.256104'/>
+          <line ref='base' x1='10' y1='7.666667' x2='10.756104' y2='4.256104'/>
+        </g>
+        <circle ref='leftEar' className='SVG-fill-bg' cx='5.333431' cy='9.6668' r='1.3335'/>
+        <circle ref='rightEar' className='SVG-fill-bg' cx='14.666443' cy='9.6668' r='1.3335'/>
+        <circle ref='leftEye' className='SVG-fill' cx='7.8' cy='11' r='1'/>
+        <circle ref='rightEye' className='SVG-fill' cx='12.2' cy='11' r='1'/>
+        <rect ref='upperEyelids' x='6.8' y='10' className='SVG-fill-bg' width='6.4' height='0'/>
+        <rect ref='lowerEyelids' x='6.8' y='12' className='SVG-fill-bg' width='6.4' height='0'/>
+        <path ref='mouth' fill='none' className='SVG-stroke' strokeWidth='0.5' strokeLinecap='round' d='M7.818661,13.516961c0.555894,0.555903,1.668416,0.662864,2.204755,0.662864 c0.53634,0,1.648866-0.10696,2.205542-0.662864'/>
+      </SVG>
+    );
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (!SVG.ENABLED) {
+      return;
+    }
+    var played = nextProps.played;
+    if (typeof played !== 'undefined' && played !== this.props.played) {
+      this._play(played);
+    }
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this._timeout);
+    this._blinkable = false;
+    this._mounted = false;
+  }
+
+  _play(bool) {
+    //TODO
+  }
+
+  startBlinking() {
+    if(this._mounted) {
+      this._blinkable = true;
+      this._blink();
+    }
+  }
+
+  _blink() {
+    if(this._blinkable && this._mounted) {
+      var upper = this.refs.upperEyelids.getDOMNode();
+      var lower = this.refs.lowerEyelids.getDOMNode();
+      var timeline = new TimelineLite({onComplete: this._blinkTimeout});
+      timeline.add(TweenLite.to([upper, lower], 0.1, {attr: {height: 1}, autoRound: false}));
+      timeline.add(TweenLite.to(lower, 0.1, {attr: {y: 11}, autoRound: false}), 0);
+      timeline.add(TweenLite.to([upper, lower], 0.1, {attr: {height: 0}, autoRound: false}));
+      timeline.add(TweenLite.to(lower, 0.1, {attr: {y: 12}, autoRound: false}), 0.1);
+    }
+  }
+
+  _blinkTimeout() {
+    if (this._blinkable && this._mounted) {
+      this._timeout = setTimeout(this._blink, Math.random() * 1000);
+    }
+  }
+
+  tweenOn(callback) {
+    TweenLite.from(this.refs.face.getDOMNode(), 0.3, {transformOrigin:'50% 50%', scale:0});
+
+    var BACK_BASE = 1.70158;
+    var refs = this.refs;
+    var nodes = [refs.rightEye, refs.leftEye, refs.rightEar, refs.leftEar];
+    var timeline = new TimelineLite({onComplete:callback});
+    for(var i = 0, iLen = nodes.length; i < iLen; i++) {
+      var overshoot = MyMath.random(2);
+      timeline.add(TweenLite.from(nodes[i].getDOMNode(), (BACK_BASE + overshoot) * 0.2, {scale: 0, transformOrigin: '50% 50%', ease: Back.easeOut.config(BACK_BASE + overshoot)}), 0.3 + MyMath.random(0.4));
+      if (i === 1) {
+        timeline.call(this.startBlinking, null, this, timeline.duration());
+      }
+    }
+    var base = refs.base.getDOMNode();
+    var stem = refs.stem.getDOMNode();
+    var dur = timeline.duration() - 0.4;
+    timeline.add(TweenLite.from(base, 0.001, {strokeWidth: 0, autoRound: false}), dur);
+    timeline.add(TweenLite.from(base, 0.2, {attr: {x2 :10, y2: 7.666667}, ease: Cubic.easeIn}), dur);
+    dur += 0.2;
+    timeline.add(TweenLite.from(stem, 0.001, {strokeWidth: 0, autoRound: false}), dur);
+    timeline.add(TweenLite.from(stem, 0.2, {attr: {x1: 10.756104, y1: 4.256104}, ease: Cubic.easeOut}), dur);
+    dur += 0.2;
+    timeline.add(TweenLite.from(refs.dingleberry.getDOMNode(), 0.7, {transformOrigin: '50% 50%', scale: 0, ease: Back.easeOut.config(5)}), dur);
+    timeline.add(TweenLite.from(refs.mouth.getDOMNode(), 0.2, {drawSVG: 0, ease: Cubic.easeInOut}), dur);
+  }
+
+  tweenOff(callback) {
+    this._blinkable = false;
+    var refs = this.refs;
+    var nodes = [refs.face, refs.rightEar, refs.leftEar, refs.rightEye, refs.leftEye, refs.dingleberry];
+    var timeline = new TimelineLite({onComplete: callback});
+    var lower = refs.lowerEyelids.getDOMNode();
+    timeline.add(TweenLite.to([refs.upperEyelids.getDOMNode(), lower], 0, {attr: {height: 0}}));
+    timeline.add(TweenLite.to(lower, 0, {attr: {y: 12}}));
+    timeline.add(TweenLite.to(this.refs.stem.getDOMNode(), 0.15, {attr: {x1:10.756104, y1: 4.256104}, ease: Cubic.easeIn}), 0);
+    timeline.add(TweenLite.to(this.refs.stem.getDOMNode(), 0.001, {strokeWidth: 0, autoRound: false}));
+    timeline.add(TweenLite.to(this.refs.base.getDOMNode(), 0.15, {attr: {x2: 10, y2: 7.666667}, transformOrigin: '50% 50%', ease: Cubic.easeOut}));
+    timeline.add(TweenLite.to(this.refs.base.getDOMNode(), 0.001, {strokeWidth: 0, autoRound: false}));
+
+    for(var i = 0, iLen = nodes.length; i < iLen; i++) {
+      timeline.add(TweenLite.to(nodes[i].getDOMNode(), 0.3, {scale: 0, transformOrigin: '50% 50%', ease: Cubic.easeOut}), 0);
+    }
+    timeline.add(TweenLite.to(this.refs.mouth.getDOMNode(), 0.2, {drawSVG: 0, ease: Cubic.easeInOut}), 0);
+  }
+}
+
+SnooIcon.defaultProps = {
+  played: false,
+};
+
+function SnooIconFactory(app) {
+  SVG = SVGFactory(app);
+  return app.mutate('core/components/SnooIcon', SnooIcon);
+}
+
+export default SnooIconFactory;
