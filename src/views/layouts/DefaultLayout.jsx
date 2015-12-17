7529b920b401b61c951d635ac33308154073f245
David Wick <wick.d.spencer@gmail.com>
2015-12-17 11:40:31 -0800

    DNT: Disable google analytics for DNT users

--- src/views/layouts/DefaultLayout.jsx
+++ src/views/layouts/DefaultLayout.jsx
@@ -45,13 +45,15 @@ function DefaultLayout  (props) {
 
     let trackingCode = `
       <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      if (!window.DO_NOT_TRACK) {
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-      ga('create', '${googleAnalyticsId}', 'auto', {'sampleRate': 50});
-      ga('send', 'pageview');
+        ga('create', '${googleAnalyticsId}', 'auto', {'sampleRate': 50});
+        ga('send', 'pageview');
+      }
       </script>
     `;
 
