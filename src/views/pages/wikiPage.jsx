2b28a35faac4f91fca12b1129b0fb2ba74caaf55
adam <adam@reddit.com>
2016-01-21 12:24:28 -0800

    WikiPage refactor make content method into smaller methods

--- src/views/pages/wikiPage.jsx
+++ src/views/pages/wikiPage.jsx
@@ -23,139 +23,163 @@ class WikiPageComp extends BasePage {
     ctx: React.PropTypes.object.isRequired,
   };
 
-  makeContent(props, data) {
-    const compact = props.compact;
-    let content;
+  renderWikiRevisions(wikiPage) {
+    if (!wikiPage.revisions.length) {
+      return (
+        <div className='wikiPage-container'>
+          <p>No recent revisions to show.</p>
+        </div>
+      );
+    }
+
+    const revisions = wikiPage.revisions.map((wikiRevision, i) => {
+      const { author, page, timestamp } = wikiRevision;
+      return (
+        <tr key={ i }>
+          <td className='wikiPage-revision-author'>{ author ? author.name : 'Unknown' }</td>
+          <td>{ page }</td>
+          <td className='wikiPage-revision-date'>{ moment(timestamp * 1000).fromNow() }</td>
+        </tr>
+      );
+    });
+
+    return (
+      <div className='wikiPage-container'>
+        <h2>Revisions</h2>
+        <table className='wikiPage-revision-table'>
+          <tbody>
+            <tr>
+              <th>User</th>
+              <th>Page</th>
+              <th>When</th>
+            </tr>
+            { revisions }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+  renderConversations(wikiPage, user, props) {
+    const { conversations } = wikiPage;
+    if (!conversations || !conversations.length) {
+      return (
+        <div className='wikiPage-container'>
+          <p>No discussions about this page.</p>
+        </div>
+      );
+    }
+
+    return (
+      <ListingContainer
+        {...props}
+        compact={ props.compact }
+        user={ user || null }
+        showAds={ false }
+        listings={ conversations }
+      />
+    );
+  }
+
+  renderWikiPageList(wikiPage) {
+    const list = wikiPage.pages.map((link, i) => {
+      return (
+        <li key={ i }>
+          <a href={ link }>{ link }</a>
+        </li>
+      );
+    });
+
+    return (
+      <div className='wikiPage-container'>
+        <ul className='list-unstyled'>
+          { list }
+        </ul>
+      </div>
+    );
+  }
+
+  renderWikiPage(wikiPage) {
+    const { content_md, revision_by, revision_date } = wikiPage;
+    const body = content_md || 'Nothing here...';
+    const editor = revision_by ? revision_by.name: 'Unknown';
+    const date = moment(revision_date * 1000).fromNow();
+
+    return (
+      <div>
+        <div className='wikiPage-container'>
+          <div
+            className='wikiPage-html'
+            dangerouslySetInnerHTML={ { __html: process(mobilify(body)) } }
+          />
+        </div>
+        <p className='wikiPage-last-edit'>
+          Revision by
+          <span className='bold'> { editor }</span> - { date }
+        </p>
+      </div>
+    );
+  }
+
+  renderWikiSettings(wikiPage, wikiPath) {
+    const { pageEditorsList, editingPermissionLevel, listedInPagesIndex } = wikiPage;
+    const editors = pageEditorsList.map((editor, i) => {
+      return (
+        <li key= { i } >{ editor.name }</li>
+      );
+    });
+
+    const showInIndex = listedInPagesIndex ? 'true' : 'false';
+
+    return (
+      <div className='wikiPage-container'>
+        <div>
+          <h2 className='wikiPage-settings-title'>Settings for { wikiPath }</h2>
+          <p>
+            Page editing permissions:
+            <span className='bold'> { editingPermissionLevel }</span>
+          </p>
+          <p>
+            show this page on the list of wiki pages:
+            <span className='bold'> { showInIndex }</span>
+          </p>
+          <h4>Authorized to edit this page:</h4>
+          <ul className='list-unstyled'>
+            { editors }
+          </ul>
+        </div>
+      </div>
+    );
+  }
+
+  makeContent(props, data, wikiPath) {
+    const { wikiPage, user } = data;
 
     switch (data.wikiPage._type) {
       case PAGETYPES.LISTING:
-        const { conversations } = data.wikiPage;
-        if (conversations && conversations.length) {
-          content = (
-            <ListingContainer
-              {...props}
-              compact={ compact }
-              user={ data.user || null }
-              showAds={ false }
-              listings={ data.wikiPage.conversations }
-            />
-          );
-        } else {
-          content = (
-            <div className='wikiPage-container'>
-              <p>No discussions about this page.</p>
-            </div>
-          );
-        }
-        break;
+        return this.renderConversations(wikiPage, user, props);
       case PAGETYPES.WIKI_REVISION:
-        const revisions = data.wikiPage.revisions.map((wikiRevision, i) => {
-          const { author, page, timestamp } = wikiRevision;
-          return (
-            <tr key={ i }>
-              <td className='wikiPage-revision-author'>{ author ? author.name : 'Unknown' }</td>
-              <td>{ page }</td>
-              <td className='wikiPage-revision-date'>{ moment(timestamp * 1000).fromNow() }</td>
-            </tr>
-          );
-        });
-
-        if (revisions.length) {
-          content = (
-            <div className='wikiPage-container'>
-              <h2>Revisions</h2>
-              <table className='wikiPage-revision-table'>
-                <tbody>
-                  <tr>
-                    <th>User</th>
-                    <th>Page</th>
-                    <th>When</th>
-                  </tr>
-                  { revisions }
-                </tbody>
-              </table>
-            </div>
-          );
-        } else {
-          content = (
-            <div className='wikiPage-container'>
-              <p>No recent revisions to show.</p>
-            </div>
-          );
-        }
-        break;
+        return this.renderWikiRevisions(wikiPage);
       case PAGETYPES.WIKIPAGE_LISTING:
-        const list = data.wikiPage.pages.map((link, i) => {
-          return (
-            <li key={ i }>
-              <a href={ link }>{ link }</a>
-            </li>
-          );
-        });
-
-        content = (
-          <div className='wikiPage-container'>
-            <ul className='list-unstyled'>
-              { list }
-            </ul>
-          </div>
-        );
-
-        break;
+        return this.renderWikiPageList(wikiPage);
       case PAGETYPES.WIKIPAGE:
-        const { content_md, revision_by, revision_date } = data.wikiPage;
-        const body = content_md || 'Nothing here...';
-        const editor = revision_by ? revision_by.name: 'Unknown';
-        const date = moment(revision_date * 1000).fromNow();
-
-        content = (
-          <div>
-            <div className='wikiPage-container'>
-              <div
-                className='wikiPage-html'
-                dangerouslySetInnerHTML={ { __html: process(mobilify(body)) } }
-              />
-            </div>
-            <p className='wikiPage-last-edit'>
-              Revision by <span className='bold'>{ editor }</span> - { date }
-            </p>
-          </div>
-        );
-        break;
+        return this.renderWikiPage(wikiPage);
       case PAGETYPES.WIKIPAGE_SETTINGS:
-        const { pageEditorsList, editingPermissionLevel, listedInPagesIndex } = data.wikiPage;
-        const editors = pageEditorsList.map((editor, i) => {
-          return (
-            <li key= { i } >{ editor.name }</li>
-          );
-        });
-
-        const wikiPath = props.ctx.params.wikiPath;
-        const showInIndex = listedInPagesIndex ? 'true' : 'false';
-
-        content = (
-          <div className='wikiPage-container'>
-            <div>
-              <h2 className='wikiPage-settings-title'>Settings for { wikiPath }</h2>
-              <p>
-                Page editing permissions:
-                <span className='bold'> { editingPermissionLevel }</span>
-              </p>
-              <p>
-                show this page on the list of wiki pages:
-                <span className='bold'> { showInIndex }</span>
-              </p>
-              <h4>Authorized to edit this page:</h4>
-              <ul className='list-unstyled'>
-                { editors }
-              </ul>
-            </div>
-          </div>
-        );
-        break;
+        return this.renderWikiSettings(wikiPage, wikiPath);
     }
+  }
 
-    return content;
+  maybeRenderDiscussLink(wikiPath, discussActive, urlPrefix) {
+    return wikiPath ? (
+      <li className={ `TextSubNav-li ${discussActive}` } >
+        <a
+          className='TextSubNav-a'
+          href={ `${urlPrefix}/wiki/discussions/${wikiPath}` }
+        >
+          Discussions
+        </a>
+      </li>
+    ) : null;
   }
 
   render() {
@@ -167,29 +191,19 @@ class WikiPageComp extends BasePage {
       );
     }
 
-    const content = this.makeContent(this.props, data);
+    const { wikiPath, subPath } = this.props.ctx.params;
+    const content = this.makeContent(this.props, data, wikiPath);
 
     const subreddit = this.props.subredditName;
     const urlPrefix = subreddit ? `/r/${subreddit}` : '';
 
-    const { wikiPath, subPath } = this.props.ctx.params;
     const revisionActive = subPath === 'revisions' ? 'active' : '';
     const pagesActive = subPath === 'pages' ? 'active' : '';
     const discussActive = subPath === 'discussions' ? 'active' : '';
 
-    let discussionsLink;
-    if (wikiPath) {
-      discussionsLink = (
-        <li className={ `TextSubNav-li ${discussActive}` } >
-          <a
-            className='TextSubNav-a'
-            href={ `${urlPrefix}/wiki/discussions/${wikiPath}` }
-          >
-            Discussions
-          </a>
-        </li>
-      );
-    }
+    const discussLink = this.maybeRenderDiscussLink(wikiPath,
+                                                    discussActive,
+                                                    urlPrefix);
 
     const path = wikiPath ? `/${wikiPath}` : '';
 
@@ -204,7 +218,7 @@ class WikiPageComp extends BasePage {
               Revisions
             </a>
           </li>
-          { discussionsLink }
+          { discussLink }
           <li className={ `TextSubNav-li ${pagesActive}` } >
             <a
               className='TextSubNav-a'
