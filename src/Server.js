d6795e68f0b41a706e7f491a2837a6c8c03b5659
Jack Lawson <ajacksified@gmail.com>
2016-05-16 11:43:59 -0700

    Server clustering

--- src/Server.js
+++ src/Server.js
@@ -3,6 +3,9 @@ import Server from '@r/platform/server';
 import APIOptions from '@r/api-client';
 import KoaStatic from 'koa-static';
 
+import cluster from 'cluster';
+import { cpus } from 'os';
+
 import routes from 'app/router';
 import main from 'server/templates/main';
 import allReducers from 'app/reducers';
@@ -19,6 +22,8 @@ import dispatchInitialCollapsedComments from
 const binFiles = KoaStatic('bin');
 const assetFiles = KoaStatic('assets');
 
+const processes = process.env.PROCESSES || cpus().length;
+
 // set up the private API
 const ConfigedAPIOptions = {
   ...APIOptions,
@@ -28,28 +33,61 @@ const ConfigedAPIOptions = {
   clientSecret: process.env.OAUTH_SECRET,
 };
 
-console.log(ConfigedAPIOptions);
+export function startServer() {
+  console.log(`Started server at PID ${process.pid}`);
+  // Create and launch the server
+  return Server({
+    routes,
+    template: main,
+    reducers: allReducers,
+    dispatchBeforeNavigation: async (ctx, dispatch/*, getState, utils*/) => {
+      await dispatchSession(ctx, dispatch, ConfigedAPIOptions);
+      await dispatchInitialTheme(ctx, dispatch);
+      await dispatchInitialCollapsedComments(ctx, dispatch);
+      await dispatchInitialCompact(ctx, dispatch);
+    },
+    preRouteServerMiddleware: [
+      binFiles,
+      assetFiles,
+    ],
+    getServerRouter: router => {
+      // private routes for login, logout, register, and token refresh
+      loginproxy(router, ConfigedAPIOptions);
+      logoutproxy(router, ConfigedAPIOptions);
+      // registerproxy(router, ConfigedAPIOptions);
+      refreshproxy(router, ConfigedAPIOptions);
+    },
+  })();
+}
 
-// Create and launch the server
-Server({
-  routes,
-  template: main,
-  reducers: allReducers,
-  dispatchBeforeNavigation: async (ctx, dispatch/*, getState, utils*/) => {
-    await dispatchSession(ctx, dispatch, ConfigedAPIOptions);
-    await dispatchInitialTheme(ctx, dispatch);
-    await dispatchInitialCollapsedComments(ctx, dispatch);
-    await dispatchInitialCompact(ctx, dispatch);
-  },
-  preRouteServerMiddleware: [
-    binFiles,
-    assetFiles,
-  ],
-  getServerRouter: router => {
-    // private routes for login, logout, register, and token refresh
-    loginproxy(router, ConfigedAPIOptions);
-    logoutproxy(router, ConfigedAPIOptions);
-    // registerproxy(router, ConfigedAPIOptions);
-    refreshproxy(router, ConfigedAPIOptions);
-  },
-})();
+export function workerExit(failedProcesses, worker) {
+  if (failedProcesses < 20) {
+    console.log(`Worker ${worker.process.pid} died, restarting.`);
+    cluster.fork();
+    failedProcesses++;
+  } else {
+    console.log('Workers died too many times, exiting.');
+    process.exit();
+  }
+}
+
+export function startCluster() {
+  let failedProcesses = 0;
+
+  cluster.setupMaster();
+
+  for (let i = 0; i < processes; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker) => workerExit(failedProcesses, worker));
+  cluster.on('exit', () => failedProcesses++);
+
+  console.log(`Started cluster with ${processes} processes.`);
+}
+
+if (cluster.isMaster && processes > 1) {
+  startCluster();
+} else {
+  startServer();
+}
