424048651d155f371a8bda3a71b69a015be9d89c
Ryan Schwers <schwers.r@gmail.com>
2016-05-17 15:02:45 -0700

    Add logic to convert reddit dot com cookies

--- src/server/session/dispatchSession.js
+++ src/server/session/dispatchSession.js
@@ -11,12 +11,23 @@ export default async (ctx, dispatch, apiOptions) => {
   // if the session is malformed somehow, the catch will trigger when trying
   // to access it
   const tokenCookie = ctx.cookies.get('token');
-  if (!tokenCookie) {
+  const redditSessionExists = ctx.cookies.get('reddit_session');
+  if (!(tokenCookie || redditSessionExists)) {
     return;
   }
 
-  const sessionData = JSON.parse(atob(tokenCookie));
-  let session = new Session(sessionData);
+  let session;
+  let sessionData;
+
+  if (tokenCookie) {
+    sessionData = JSON.parse(atob(tokenCookie));
+    session = new Session(sessionData);
+  } else {
+    // try to convert reddit session cookie
+    const cookies = ctx.headers.cookie.replace(/__cf_mob_redir=1/, '__cf_mob_redir=0').split(';');
+    sessionData = await PrivateAPI.convertCookiesToAuthToken(apiOptions, cookies);
+    session = new Session(sessionData);
+  }
 
   // if the session is invalid, try to use the refresh token to grab a new
   // session.
