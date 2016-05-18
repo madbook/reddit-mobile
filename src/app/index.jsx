d735c47368f148e1061a99d8f5b7d406b14c147d
Ryan Schwers <schwers.r@gmail.com>
2016-05-18 15:54:06 -0700

    Move components for side effects into side-effect-components/

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -6,9 +6,9 @@ import { UrlSync } from '@r/platform/components';
 import { AppMainPage } from './pages/AppMain';
 import AppOverlayMenu from './components/AppOverlayMenu';
 import { AppTopNav } from './components/AppTopNav';
-import { CookieSync } from './CookieSync';
-import { DomModifier } from './DomModifier';
-import { LocalStorageSync } from './LocalStorageSync';
+import { CookieSync } from './side-effect-components/CookieSync';
+import { DomModifier } from './side-effect-components/DomModifier';
+import { LocalStorageSync } from './side-effect-components/LocalStorageSync';
 
 export default class App extends React.Component {
   render() {
