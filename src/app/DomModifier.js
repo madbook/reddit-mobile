de60e192bc63353ebe3a6b422934b440227190ac
Ryan Schwers <schwers.r@gmail.com>
2016-05-17 18:55:36 -0700

    Port background scroller to 2X

--- src/app/DomModifier.js
+++ src/app/DomModifier.js
@@ -1,9 +1,17 @@
 import { makeStateArchiver } from '@r/redux-state-archiver';
 import { themeClass } from 'server/templates/themeClass';
-import { themes } from 'app/constants';
+import { themes, OVERLAY_MENU_VISIBLE_CSS_CLASS } from 'app/constants';
+import { stopScroll } from 'lib/stopScroll';
+import { OVERLAY_MENU_PARAMETER } from 'app/actions/overlayMenu';
+
+const stopScrollForMenu = stopScroll(OVERLAY_MENU_VISIBLE_CSS_CLASS);
 
 const themeSelector = (state) => state.theme;
-const combiner = (theme) => ({ theme });
+const overlayOpenSelector = (state) => {
+  return !!state.platform.currentPage.queryParams[OVERLAY_MENU_PARAMETER];
+};
+
+const combiner = (theme, overlayOpen ) => ({ theme, overlayOpen });
 
 const updateTheme = ($body, newTheme) => {
   const nextThemeClass = themeClass(newTheme);
@@ -16,18 +24,46 @@ const updateTheme = ($body, newTheme) => {
   $body.classList.add(nextThemeClass);
 };
 
+const updateOverlayScrollStopper = ($body, open) => {
+  // Scrolling on Safari is weird, possibly iOS 9. Overflow hidden doesn't
+  // prevent the page background from scrolling as you'd expect.
+  // When we're on Safari we do a fancy check to stop touchmove events
+  // from scrolling the background.
+  // We don't use position: fixed becuase the repaint from changing position
+  // is slow in safari. Plus there's extra bookkeeping for preserving the
+  // scroll position.
+  if (open) {
+    if ($body.classList.contains(OVERLAY_MENU_VISIBLE_CSS_CLASS)) {
+      return;
+    }
+
+    $body.classList.add(OVERLAY_MENU_VISIBLE_CSS_CLASS);
+    $body.addEventListener('touchmove', stopScrollForMenu);
+  } else {
+    $body.classList.remove(OVERLAY_MENU_VISIBLE_CSS_CLASS);
+    $body.removeEventListener('touchmove', stopScrollForMenu);
+  }
+};
+
 let $body;
 const archiver = (data) => {
   if (!$body) { $body = document.body; }
 
-  const { theme } = data;
+  // everthing from here down depends on the classList api
+  if (!$body.classList) { return; }
+
+  const { theme, overlayOpen } = data;
   if (theme) {
     updateTheme($body, data.theme);
+  }
+
+  if (typeof overlayOpen !== 'undefined') {
+    updateOverlayScrollStopper($body, overlayOpen);
   }
 };
 
 export const DomModifier = makeStateArchiver(
-  [ themeSelector ],
+  [ themeSelector, overlayOpenSelector ],
   combiner,
   archiver,
 );
---
+++ src/lib/stopScroll.js
@@ -0,0 +1,40 @@
+import { throttle } from 'lodash/function';
+
+const elementCanScroll = (el) => {
+  const top = el.scrollTop;
+
+  if (top <= 0) {
+    el.scrollTop = 1;
+    return false;
+  }
+
+  const totalScroll = top + el.offsetHeight;
+  if (totalScroll === el.scrollHeight) {
+    el.scrollTop = top - 1;
+    return false;
+  }
+
+  return true;
+};
+
+export const stopScroll = (elClass, delay=50) => {
+  return throttle(function stopScroll(e) {
+    let touchMoveAllowed = false;
+    let target = e.target;
+
+    while (target !== null) {
+      if (target.classList && target.classList.contains(elClass)) {
+        if (elementCanScroll(target)) {
+          touchMoveAllowed = true;
+        }
+        break;
+      }
+
+      target = target.parentNode;
+    }
+
+    if (!touchMoveAllowed) {
+      e.preventDefault();
+    }
+  }, delay);
+};
