3eea8ccfdb618bbbde89e8b460e38fd2ec5f55d8
Joshua Uziel <uzi@reddit.com>
2016-06-01 11:57:10 -0700

    Fix a key typo in the apple-app-site-association

--- src/server/routes.es6.js
+++ src/server/routes.es6.js
@@ -12,7 +12,7 @@ const appleAppSiteAssociation = JSON.stringify({
   },
   applinks: {
     apps: [],
-    detail: [
+    details: [
       {
         appID: '2TDUX39LX8.com.reddit.Reddit',
         paths: [ '/', '/r/*', '/u/*', '/user/*' ],
