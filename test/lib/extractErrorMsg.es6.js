f915124f9ca1f6d81997bc092dec57c0a37b9199
William Ting <wting@reddit.com>
2016-07-22 18:31:51 -0700

    Minor cleanup.

--- test/lib/extractErrorMsg.es6.js
+++ test/lib/extractErrorMsg.es6.js
@@ -9,26 +9,26 @@ const GENERIC_MESSAGE = 'There was a problem';
 chai.use(sinonChai);
 
 describe('lib: extractErrorMsg', () => {
-  it('returns error messages nested in a compolete, 2-deep array', () => {
+  it('returns error messages nested in a complete, 2-deep array', () => {
     expect(extractErrorMsg({errors: [['error type', TEST_MESSAGE]]}))
       .to.equal(TEST_MESSAGE);
   });
-  
-  it('returns a any message found in error.message', () => {
+
+  it('returns any message found in error.message', () => {
     expect(extractErrorMsg({message: TEST_MESSAGE}))
     .to.equal(TEST_MESSAGE);
   });
-  
+
   it('returns a generic message for all other cases', () => {
     expect(extractErrorMsg({errors: []}))
       .to.equal(GENERIC_MESSAGE);
-      
+
     expect(extractErrorMsg({errors: [[]]}))
       .to.equal(GENERIC_MESSAGE);
-      
+
     expect(extractErrorMsg(null))
       .to.equal(GENERIC_MESSAGE);
-      
+
     expect(extractErrorMsg({}))
       .to.equal(GENERIC_MESSAGE);
   });
