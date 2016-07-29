f26d0e3226e54c766ee87facae7c21f2a741b627
Phil Aquilina <philaquilina@gmail.com>
2016-07-28 17:04:42 -0700

    remove svg compatibility check

--- src/app/components/SVG/index.jsx
+++ src/app/components/SVG/index.jsx
@@ -1,59 +1,39 @@
 import React from 'react';
 
+const T = React.PropTypes;
+
 const _NS = 'http://www.w3.org/2000/svg';
+const ICON_SIZE = 20;
 
-function SVG (props) {
-  const width = props.width || SVG.ICON_SIZE;
-  const height = props.height || SVG.ICON_SIZE;
-
-  if (SVG.ENABLED) {
-    const move = props.move;
-    const out = props.out;
-
-    return (
-      <svg
-        className={ props.className }
-        version='1.1'
-        xmlns={ _NS }
-        x='0px'
-        y='0px'
-        width={ `${width}px` }
-        height={ `${height}px` }
-        viewBox={ `0 0 ${width} ${height}` }
-        onMouseMove={ move }
-        onMouseLeave={ out }
-      >
-        { props.children }
-      </svg>
-    );
-  }
-
-  const fallbackIcon = props.fallbackIcon;
-  if (fallbackIcon) {
-    return <figure className={ `${props.className} ${fallbackIcon}` }/>;
-  }
-
-  const fallbackText = props.fallbackText;
-  if (fallbackText) {
-    return <span className='fallbackText'>{ fallbackText }</span>;
-  }
-
-  const fallbackImg = props.fallbackImg;
-  if (fallbackImg) {
-    return <img className='fallbackImg' src={ fallbackImg } width={ width } height={ height }/>;
-  }
-
-  const Fallback = props.fallbackComponent;
-  if (Fallback) {
-    return <Fallback/>;
-  }
-
-  return <span className='placeholder'/>;
+export default function SVG(props) {
+  const { move, out, className, children, width, height } = props;
+  return (
+    <svg
+      className={ className }
+      version='1.1'
+      xmlns={ _NS }
+      x='0px'
+      y='0px'
+      width={ `${width}px` }
+      height={ `${height}px` }
+      viewBox={ `0 0 ${width} ${height}` }
+      onMouseMove={ move }
+      onMouseLeave={ out }
+    >
+      { children }
+    </svg>
+  );
 }
 
-SVG.ENABLED = typeof document !== 'undefined' &&
-              (!!document.createElementNS && !!document.createElementNS(_NS, 'svg').createSVGRect);
+SVG.propTypes = {
+  move: T.func,
+  out: T.func,
+  className: T.string,
+  width: T.number,
+  height: T.number,
+};
 
-SVG.ICON_SIZE = 20;
-
-export default SVG;
+SVG.defaultProps = {
+  width: ICON_SIZE,
+  height: ICON_SIZE,
+};
