899678fa5648b9389a98d468ed3da373380e51c2
umbrae <umbrae@reddit.com>
2016-08-30 16:21:50 -0700

    Compose: add escape hatch link
    Compose is not yet ready on mweb, so we add a link to desktop
    compose as an alternative.

--- src/views/pages/messageCompose.jsx
+++ src/views/pages/messageCompose.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import config from '../../config';
+
 import MessageNav from '../components/MessageNav';
 import BasePage from './BasePage';
+
+const COMPOSE_LINK = `${config.reddit}/message/compose`;
 
 class MessageComposePage extends BasePage {
   render () {
@@ -13,7 +17,11 @@ class MessageComposePage extends BasePage {
             <div className='row'>
               <div className='col-xs-12 col-sm-6'>
                 <div className='well well-lg'>
-                  <p>Sorry, this isn’t ready yet!</p>
+                  <h3>
+                    Sorry, this isn’t ready yet! You can
+                    <a href={ COMPOSE_LINK }> compose a message on the desktop site </a>
+                    instead.
+                  </h3>
                 </div>
               </div>
             </div>
