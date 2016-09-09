6bf0ad99e6cd2334dbe359d43757d7f3df36aa6b
Ryan Schwers <schwers.r@gmail.com>
2016-09-09 14:47:26 -0700

    Count filtered r/all as a fake subreddit

--- src/lib/isFakeSubreddit.es6.js
+++ src/lib/isFakeSubreddit.es6.js
@@ -4,5 +4,9 @@ const fakeSubs = ['all', 'mod', 'friends'].concat(randomSubs);
 export { fakeSubs, randomSubs };
 
 export default function isFakeSubreddit(subredditName) {
-  return !!subredditName && (subredditName.indexOf('+') > -1 || fakeSubs.includes(subredditName));
+  return !!subredditName && (
+    subredditName.indexOf('+') > -1 ||
+    subredditName.indexOf('-') > -1 ||
+    fakeSubs.includes(subredditName)
+  );
 }
