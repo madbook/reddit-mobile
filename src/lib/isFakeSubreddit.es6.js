2d8b5dc24fdc677d634edb9ae24f35cdcc8d9aa6
Joshua Uziel <uzi@reddit.com>
2016-09-12 12:26:33 -0700

    Revert "Count filtered r/all as a fake subreddit"
    This reverts commit 6bf0ad99e6cd2334dbe359d43757d7f3df36aa6b.

--- src/lib/isFakeSubreddit.es6.js
+++ src/lib/isFakeSubreddit.es6.js
@@ -4,9 +4,5 @@ const fakeSubs = ['all', 'mod', 'friends'].concat(randomSubs);
 export { fakeSubs, randomSubs };
 
 export default function isFakeSubreddit(subredditName) {
-  return !!subredditName && (
-    subredditName.indexOf('+') > -1 ||
-    subredditName.indexOf('-') > -1 ||
-    fakeSubs.includes(subredditName)
-  );
+  return !!subredditName && (subredditName.indexOf('+') > -1 || fakeSubs.includes(subredditName));
 }
