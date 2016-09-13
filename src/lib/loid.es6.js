d04cd7df38c32beb0f09093519fd0aaade06fdc7
Niranjan Ramadas <nrbramadas@gmail.com>
2016-09-13 15:27:08 -0700

    Use loids generated by the api
    This reverts commit 39aa137316c001da3185c3d808c0fcd383a63af8.

--- src/lib/loid.es6.js
+++ src/lib/loid.es6.js
@@ -1,15 +1,17 @@
-import randomString from './randomString';
+import config from '../config';
 
-function setLoggedOutCookies(cookies, app) {
-  const loid = randomString(18);
-  const loidcreated = (new Date()).toISOString();
-
+function setLoggedOutCookies(cookies, app, loid, loidcreated) {
   const options = {
     secure: app.getConfig('https'),
     secureProxy: app.getConfig('httpsProxy'),
     httpOnly: false,
     maxAge: 1000 * 60 * 60 * 24 * 365 * 2,
   };
+
+  if (app.config.origin.indexOf('localhost') === -1) {
+    // We set the domain here to be `reddit.com` (for production reddit)
+    options.domain = config.rootReddit;
+  }
 
   cookies.set('loid', loid, options);
   cookies.set('loidcreated', loidcreated, options);
