39aa137316c001da3185c3d808c0fcd383a63af8
Niranjan Ramadas <nrbramadas@gmail.com>
2016-09-13 15:15:24 -0700

    Revert "Use loids generated by the api"
    This reverts commit 61f659eafc8a1da34e696f6db44c13f974ea8b20.

--- src/lib/loid.es6.js
+++ src/lib/loid.es6.js
@@ -1,17 +1,15 @@
-import config from '../config';
+import randomString from './randomString';
 
-function setLoggedOutCookies(cookies, app, loid, loidcreated) {
+function setLoggedOutCookies(cookies, app) {
+  const loid = randomString(18);
+  const loidcreated = (new Date()).toISOString();
+
   const options = {
     secure: app.getConfig('https'),
     secureProxy: app.getConfig('httpsProxy'),
     httpOnly: false,
     maxAge: 1000 * 60 * 60 * 24 * 365 * 2,
   };
-
-  if (app.config.origin.indexOf('localhost') === -1) {
-    // We set the domain here to be `reddit.com` (for production reddit)
-    options.domain = config.rootReddit;
-  }
 
   cookies.set('loid', loid, options);
   cookies.set('loidcreated', loidcreated, options);
