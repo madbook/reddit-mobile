1f0b9cf6d46be45d122029bcad6dbf8f948d7628
Ryan Schwers <schwers.r@gmail.com>
2016-09-15 15:22:32 -0700

    Fetch user in handlerCommon again.
    This was removed in the loid patches, but login relies on this happening
    on every route to ensure the user is loaded. We could add it only to the
    login endpoint but then if it fails, we won't retry. Restoring
    it to handlerCommon is the best approach for now.

--- src/app/router/handlers/handlerCommon.js
+++ src/app/router/handlers/handlerCommon.js
@@ -1,7 +1,9 @@
 import * as subscribedSubredditsActions from 'app/actions/subscribedSubreddits';
 import * as preferenceActions from 'app/actions/preferences';
+import * as userActions from 'app/actions/user';
 
 export const fetchUserBasedData = (dispatch) => {
   dispatch(subscribedSubredditsActions.fetchSubscribedSubreddits(true));
+  dispatch(userActions.fetchMyUser());
   dispatch(preferenceActions.fetch());
 };
