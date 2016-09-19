8baa1e832366bd410df75312d9b7b5d40202c1e3
Joshua Uziel <uzi@reddit.com>
2016-09-19 11:28:45 -0700

    NSFW Interstitial doesn't need a redirect
    When the interstitial is being shown, we're already
    being routed to the page we want. With the code as
    is, we're getting redirected back to the root.

--- src/app/router/handlers/SetOver18.js
+++ src/app/router/handlers/SetOver18.js
@@ -1,14 +1,8 @@
 import { BaseHandler, METHODS } from '@r/platform/router';
-import * as platformActions from '@r/platform/actions';
-
 import * as preferenceActions from 'app/actions/preferences';
 
 export default class SetOver18Handler extends BaseHandler {
-  async [METHODS.POST](dispatch, getState) {
+  async [METHODS.POST](dispatch) {
     dispatch(preferenceActions.setOver18());
-
-    // redirect to current page
-    const { currentPage } = getState().platform;
-    dispatch(platformActions.navigateToUrl(METHODS.GET, currentPage.referrer));
   }
 }
