4064459e740db390eab8f66307e9e51d85c31631
Joshua Uziel <uzi@reddit.com>
2016-10-12 17:48:43 -0700

    Users have to be logged in to toggle subscription
    In the case that they're not logged in, we send
    then to the registration page.

--- src/app/router/handlers/ToggleSubredditSubscription.js
+++ src/app/router/handlers/ToggleSubredditSubscription.js
@@ -1,8 +1,16 @@
 import { BaseHandler, METHODS } from '@r/platform/router';
+import * as platformActions from '@r/platform/actions';
+
+import { LOGGEDOUT_REDIRECT } from 'app/constants';
 import * as subscriptionActions from 'app/actions/subscribedSubreddits';
 
 export default class ToggleSubredditSubscription extends BaseHandler {
-  async [METHODS.POST](dispatch) {
+  async [METHODS.POST](dispatch, getState) {
+    const state = getState();
+    if (!state.session.isValid) {
+      return dispatch(platformActions.setPage(LOGGEDOUT_REDIRECT));
+    }
+
     dispatch(subscriptionActions.toggleSubscription(this.bodyParams));
   }
 }
