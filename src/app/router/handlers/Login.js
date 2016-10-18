555cd37edb10b0e27d1ccafadabf46e703e7e12e
Joshua Uziel <uzi@reddit.com>
2016-10-17 17:18:26 -0700

    Log the login_attempt and register_attempt events

--- src/app/router/handlers/Login.js
+++ src/app/router/handlers/Login.js
@@ -6,6 +6,7 @@ import Session from 'app/models/Session';
 import * as sessionActions from 'app/actions/session';
 import * as loginActions from 'app/actions/login';
 import { getBasePayload, logClientScreenView } from 'lib/eventUtils';
+import { getEventTracker } from 'lib/eventTracker';
 
 
 export default class Login extends BaseHandler {
@@ -13,21 +14,43 @@ export default class Login extends BaseHandler {
     logClientScreenView(getBasePayload, getState());
   }
 
-  async [METHODS.POST](dispatch) {
+  async [METHODS.POST](dispatch, getState) {
     const { username, password } = this.bodyParams;
+    let successful = true;
+    let errorCode = null;
 
     try {
       const newSession = await Session.fromLogin(username, password);
       dispatch(sessionActions.setSession(newSession));
       dispatch(loginActions.loggedIn());
-      dispatch(platformActions.navigateToUrl(METHODS.GET, '/'));
+
+      // This is awaited to guarantee the user is loaded for event logging
+      await dispatch(platformActions.navigateToUrl(METHODS.GET, '/'));
 
     } catch (e) {
+      successful = false;
       if (e instanceof errors.ValidationError && e.errors && e.errors[0]) {
-        dispatch(sessionActions.sessionError(e.errors[0].error));
+        errorCode = e.errors[0].error;
+        dispatch(sessionActions.sessionError(errorCode));
       } else {
         throw e;
       }
     }
+
+    logAttempt(this.bodyParams, successful, errorCode, getState());
   }
+}
+
+function logAttempt(data, successful, errorCode, state) {
+  const payload = {
+    ...getBasePayload(state),
+    successful,
+    user_name: data.username,
+  };
+
+  if (!successful && errorCode) {
+    payload.process_notes = errorCode;
+  }
+
+  getEventTracker().track('login_events', 'cs.login_attempt', payload);
 }
--- src/app/router/handlers/Register.js
+++ src/app/router/handlers/Register.js
@@ -6,6 +6,7 @@ import { registerUser } from 'app/models/Register';
 import * as sessionActions from 'app/actions/session';
 import * as loginActions from 'app/actions/login';
 import { getBasePayload, logClientScreenView } from 'lib/eventUtils';
+import { getEventTracker } from 'lib/eventTracker';
 
 
 export default class Register extends BaseHandler {
@@ -13,21 +14,46 @@ export default class Register extends BaseHandler {
     logClientScreenView(getBasePayload, getState());
   }
 
-  async [METHODS.POST](dispatch) {
+  async [METHODS.POST](dispatch, getState) {
     const { username, password, email, newsletter, gRecaptchaResponse } = this.bodyParams;
+    let successful = true;
+    let errorCode = null;
+
     try {
       const newSession = await registerUser(username, password, email,
                                             newsletter, gRecaptchaResponse);
       dispatch(sessionActions.setSession(newSession));
       dispatch(loginActions.loggedIn());
-      dispatch(platformActions.navigateToUrl(METHODS.GET, '/'));
+
+      // This is awaited to guarantee the user is loaded for event logging
+      await dispatch(platformActions.navigateToUrl(METHODS.GET, '/'));
 
     } catch (e) {
+      successful = false;
       if (e instanceof errors.ValidationError && e.errors && e.errors[0]) {
-        dispatch(sessionActions.sessionError(e.errors[0].error));
+        errorCode = e.errors[0].error;
+        dispatch(sessionActions.sessionError(errorCode));
       } else {
         throw e;
       }
     }
+
+    logAttempt(this.bodyParams, successful, errorCode, getState());
   }
+}
+
+function logAttempt(data, successful, errorCode, state) {
+  const payload = {
+    ...getBasePayload(state),
+    successful,
+    user_name: data.username,
+    email: data.email,
+    newsletter: data.newsletter && data.newsletter[0] === 'on',
+  };
+
+  if (!successful && errorCode) {
+    payload.process_notes = errorCode;
+  }
+
+  getEventTracker().track('login_events', 'cs.register_attempt', payload);
 }
