c563bd7a9d47c74ea7f997323f099f4aec513dd9
William Ting <wting@reddit.com>
2016-12-08 15:29:03 -0800

    [squash] Use platformActions.redirect.

--- src/app/router/handlers/UserActivityReroute.js
+++ src/app/router/handlers/UserActivityReroute.js
@@ -1,21 +1,15 @@
-import { setPage, navigateToUrl } from '@r/platform/actions';
 import { BaseHandler, METHODS } from '@r/platform/router';
+import * as platformActions from '@r/platform/actions';
 
 import UserActivityHandler from 'app/router/handlers/UserActivity';
 
 export default class UserActivityRerouteHandler extends BaseHandler {
   async [METHODS.GET](dispatch, getState) {
     const { platform: { currentPage }} = getState();
-    const { urlParams, queryParams, hashParams, referrer } = currentPage;
+    const { urlParams, queryParams } = currentPage;
     const { userName } = urlParams;
     const url = UserActivityHandler.activityUrl(userName, queryParams.activity);
 
-    if (process.env.ENV === 'client') {
-      // redirect the url and make sure platform runs the handler
-      dispatch(navigateToUrl(METHODS.GET, url, { urlParams, queryParams, hashParams }));
-    } else {
-      // redirect but don't run the handler
-      dispatch(setPage(url, { urlParams, queryParams, hashParams, referrer }));
-    }
+    dispatch(platformActions.redirect(url));
   }
 }
