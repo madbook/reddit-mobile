e5be551943b15735c7c1241aa28dabbedb9ecaf4
Ryan Schwers <schwers.r@gmail.com>
2017-01-28 23:27:13 -0800

    Add WebpackBundleAnalyzer to audit client builds

--- blueprints.config.js
+++ blueprints.config.js
@@ -13,6 +13,36 @@ function generateReleaseVersion() {
   }
 }
 
+function makeBundleAnalyzer(name, port) {
+  var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+  return new BundleAnalyzerPlugin({
+    // Can be `server`, `static` or `disabled`.
+    // In `server` mode analyzer will start HTTP server to show bundle report.
+    // In `static` mode single HTML file with bundle report will be generated.
+    // In `disabled` mode you can use this plugin to just generate Webpack Stats JSON file by setting `generateStatsFile` to `true`.
+    analyzerMode: 'server',
+    // Port that will be used in `server` mode to start HTTP server.
+    analyzerPort: port,
+    // Path to bundle report file that will be generated in `static` mode.
+    // Relative to bundles output directory.
+    reportFilename: name + '-report.html',
+    // Automatically open report in default browser
+    openAnalyzer: true,
+    // If `true`, Webpack Stats JSON file will be generated in bundles output directory
+    generateStatsFile: true,
+    // Name of Webpack Stats JSON file that will be generated if `generateStatsFile` is `true`.
+    // Relative to bundles output directory.
+    statsFilename: name + '-stats.json',
+    // Options for `stats.toJson()` method.
+    // For example you can exclude sources of your modules from stats file with `source: false` option.
+    // See more options here: https://github.com/webpack/webpack/blob/webpack-1/lib/Stats.js#L21
+    statsOptions: null,
+    // Log level. Can be 'info', 'warn', 'error' or 'silent'.
+    logLevel: 'info',
+  });
+}
+
 module.exports = function(isProduction) {
   var release = generateReleaseVersion();
   var options = {
@@ -55,6 +85,12 @@ module.exports = function(isProduction) {
       }
     }),
   ]);
+
+  if (process.env.ANALYZE_BUILD) {
+    clientConfig.webpack.plugins = clientConfig.webpack.plugins.concat([
+      makeBundleAnalyzer('client', 4000),
+    ]);
+  }
 
   return [ clientConfig, serverConfig ];
 };
