99772b204cecbec0fb8df370e5a83ca656378c47
Matt Lee <matt@reddit.com>
2017-02-08 13:27:56 -0500

    Refactor SubredditAbout component

--- src/app/components/SubredditAbout/index.js
+++ src/app/components/SubredditAbout/index.js
@@ -9,41 +9,68 @@ import { Anchor } from '@r/platform/components';
 import Loading from 'app/components/Loading';
 import RedditLinkHijacker from 'app/components/RedditLinkHijacker';
 
-const subredditDescription = descriptionHTML => (
-  <RedditLinkHijacker>
-    <div
-      className='SubredditAbout__description'
-      dangerouslySetInnerHTML={ { __html: descriptionHTML } }
-    />
-  </RedditLinkHijacker>
-);
+const T = React.PropTypes;
 
-const tryLoggingIn = () => (
-  <div className='SubredditAbout__try-logging-in'>
-    You may need to try <Anchor href='/login'>logging in</Anchor> to view this community
-  </div>
-);
-
-const subredditLoadingError = (subredditName, user) => (
-  <div className='SubredditAbout__loading-error'>
-    Sorry, there was an error loading&nbsp;
-    <Anchor href={ `/r/${subredditName}` }>
-      { `r/${subredditName}` }
-    </Anchor>
-    { user.loggedOut && tryLoggingIn() }
-  </div>
-);
-
-const SubredditAbout = (props) => {
-  const { subreddit, subredditRequest, user, subredditName } = props;
-
+/**
+ * Component for rendering the "About this community" page.
+ * @function
+ * @param {Object} props
+ * @returns React.Component
+ */
+function SubredditAbout(props) {
+  const {
+    subreddit,
+    subredditName,
+    subredditRequest,
+    user,
+  } = props;
+  
   return (
     <div className='SubredditAbout'>
-      { subreddit ? subredditDescription(subreddit.descriptionHTML)
-        : subredditRequest && subredditRequest.failed ? subredditLoadingError(subredditName, user)
-        : <Loading /> }
+    { subreddit
+      ? <RedditLinkHijacker>
+          <div
+            className='SubredditAbout__description'
+            dangerouslySetInnerHTML={ { __html: subreddit.descriptionHTML } }
+          />
+        </RedditLinkHijacker>
+      
+    : subredditRequest && subredditRequest.failed 
+      ? <div className='SubredditAbout__loading-error'>
+          Sorry, there was an error loading&nbsp;
+          <Anchor href={ `/r/${subredditName}` }>
+            { `r/${subredditName}` }
+          </Anchor>
+          { user.loggedOut &&
+              <div className='SubredditAbout__try-logging-in'>
+                You may need to try <Anchor href='/login'>logging in</Anchor> to view this community
+              </div>
+          }
+        </div>
+        
+      : <Loading />
+    }
     </div>
   );
+}
+
+SubredditAbout.propTypes = {
+  subreddit: T.shape({
+    descriptionHTML: T.string.isRequired,
+  }),
+  subredditName: T.string.isRequired,
+  subredditRequest: T.shape({
+    failed: T.bool.isRequired,
+  }),
+  user: T.shape({
+    loggedOut: T.bool.isRequired,
+  }),
+};
+
+SubredditAbout.defaultProps = {
+  subreddit: null,
+  subredditRequest: null,
+  user: null,
 };
 
 const mapStateToProps = createSelector(
