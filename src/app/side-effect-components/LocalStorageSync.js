03fb02e14c37ed8d1648b2e671be410ca18bee00
Matt Lee <matt@reddit.com>
2017-03-09 11:07:57 -0500

    Refactor LocalStorageSync component

--- src/app/side-effect-components/LocalStorageSync.js
+++ src/app/side-effect-components/LocalStorageSync.js
@@ -1,21 +1,14 @@
 import { makeLocalStorageArchiver } from './StateArchiver';
 
-const collapsedSelector = state => state.comments.collapsed;
-const expandedSelector = state => state.expandedPosts;
-const visitedPostsSelector = state => JSON.stringify(state.visitedPosts);
-const optOutsSelector = state => state.optOuts;
-
-const combiner = (collapsedComments, expandedPosts, visitedPosts, optOuts) => ({
-  collapsedComments,
-  expandedPosts,
-  visitedPosts,
-  optOuts,
-});
-
 export default makeLocalStorageArchiver(
-  collapsedSelector,
-  expandedSelector,
-  visitedPostsSelector,
-  optOutsSelector,
-  combiner,
+  state => state.comments.collapsed,
+  state => state.expandedPosts,
+  state => JSON.stringify(state.visitedPosts),
+  state => state.optOuts,
+  (collapsedComments, expandedPosts, visitedPosts, optOuts) => ({
+    collapsedComments,
+    expandedPosts,
+    visitedPosts,
+    optOuts,
+  }),
 );
