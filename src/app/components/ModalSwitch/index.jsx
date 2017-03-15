0a97be0755a4f66667f6162c2ee82ce4c8a72c3a
Matt Lee <matt@reddit.com>
2017-03-15 11:42:22 -0400

    squash: add RulesModal component to ModalSwitch

--- src/app/components/ModalSwitch/index.jsx
+++ src/app/components/ModalSwitch/index.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
 
-import ReportingModal from '../ReportingModal';
+import ReportingModal from 'app/components/ReportingModal';
+import RulesModal from 'app/components/RulesModal';
 
 import * as reportingActions from 'app/actions/reporting';
+import * as rulesModalActions from 'app/actions/rulesModal';
 
 const ModalSwitch = props => {
   switch (props.modal.type) {
     case reportingActions.MODAL_TYPE:
       return <ReportingModal { ...props.modal } />;
+    case rulesModalActions.RULES_MODAL_TYPE:
+      return <RulesModal { ...props.modal.props } />;
     default:
       return null;
   }
