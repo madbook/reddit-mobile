538d58bfef501a1ca9d02f19931360061ed504ac
Matt Lee <matt@reddit.com>
2017-03-15 11:43:38 -0400

    squash: show on submit

--- src/app/router/handlers/PostSubmit.js
+++ src/app/router/handlers/PostSubmit.js
@@ -1,7 +1,11 @@
+import { POST } from 'apiClient/models/thingTypes';
 import { BaseHandler, METHODS } from 'platform/router';
 import * as platformActions from 'platform/actions';
+import * as rulesModalActions from 'app/actions/rulesModal';
 import * as subredditActions from 'app/actions/subreddits';
-import { LOGGEDOUT_REDIRECT } from 'app/constants';
+import * as subredditRulesActions from 'app/actions/subredditRules';
+import { flags, LOGGEDOUT_REDIRECT } from 'app/constants';
+import features from 'app/featureFlags';
 import { fetchUserBasedData } from 'app/router/handlers/handlerCommon';
 import { trackPageEvents } from 'lib/eventUtils';
 
@@ -16,6 +20,38 @@ export class PostSubmitHandler extends BaseHandler {
     await fetchUserBasedData(dispatch);
 
     trackPageEvents(getState());
+
+    // Experiment to show subreddit rules at submit time.
+    const { subredditName } = state.platform.currentPage.urlParams;
+    const featureName = 'rules_modal_on_submit';
+    const key = rulesModalActions.getLocalStorageKey(featureName, subredditName);
+
+    // Disable during shell rendering, necessary to ensure that the modal hasn't
+    // been marked as "seen" in localStorage before showing
+    if (state.platform.shell) { return }
+    // Disable if we aren't in a subreddit context
+    if (!subredditName) { return }
+    // Disable if modal has been marked as "seen" in localStorage
+    if (state.rulesModal[key]) { return }
+    
+    const feature = features.withContext({ state });
+    const clickAnywhereEnabled = feature.enabled(flags.RULES_MODAL_ON_SUBMIT_CLICK_ANYWHERE);
+    const clickButtonEnabled = feature.enabled(flags.RULES_MODAL_ON_SUBMIT_CLICK_BUTTON);
+
+    // Disable if none of the relevant features are enabled
+    if (!(clickAnywhereEnabled || clickButtonEnabled)) { return }
+
+    const isRequired = clickButtonEnabled;
+    const thingType = POST;
+    const onDecline = platformActions.navigateToUrl('get', `/r/${subredditName}`);
+
+    dispatch(rulesModalActions.display(
+      featureName,
+      subredditName,
+      thingType,
+      isRequired,
+      onDecline
+    ));
   }
 }
 
