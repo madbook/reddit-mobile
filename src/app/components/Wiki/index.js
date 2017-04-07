1695a2a6501d576303b228bfe6e5044d55cd7924
adam <curioussavage@protonmail.com>
2017-04-07 11:50:11 -0700

    handle hash fragments in wiki links

--- src/app/components/Wiki/index.js
+++ src/app/components/Wiki/index.js
@@ -3,20 +3,37 @@ import './styles.less';
 import React from 'react';
 import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
+import URL from 'url';
 
 import { Anchor } from 'platform/components';
 import { makeWikiPath } from 'lib/makeWikiPath';
 import Loading from 'app/components/Loading';
 import RedditLinkHijacker from 'app/components/RedditLinkHijacker';
 
-const wikiContent = contentHTML => (
+const TOPNAV_HEIGHT = 48;
+
+const wikiContent = (contentHTML, currentPath) => (
   <RedditLinkHijacker>
     <div
       className='Wiki__content'
       dangerouslySetInnerHTML={ { __html: contentHTML } }
+      onClick={ handleClick(currentPath) }
     />
   </RedditLinkHijacker>
 );
+
+const handleClick = currentPath => e => {
+  const url = e.target.href;
+  if (url) {
+    const { path, hash } = URL.parse(url);
+    if (path === currentPath && hash) {
+      e.preventDefault();
+      const el = document.getElementById(hash.slice(1));
+      document.body.scrollTop = el.offsetTop - TOPNAV_HEIGHT;
+    }
+  }
+
+};
 
 const tryLoggingIn = () => (
   <div className='Wiki__try-logging-in'>
@@ -35,11 +52,17 @@ const wikiLoadingError = (subredditName, user) => (
 );
 
 const Wiki = (props) => {
-  const { wiki, wikiRequest, user, subredditName } = props;
+  const {
+    currentPath,
+    subredditName,
+    user,
+    wiki,
+    wikiRequest,
+  } = props;
 
   return (
     <div className='Wiki'>
-      { wiki ? wikiContent(wiki.contentHTML)
+      { wiki ? wikiContent(wiki.contentHTML, currentPath)
         : wikiRequest && wikiRequest.failed ? wikiLoadingError(subredditName, user)
         : <Loading /> }
     </div>
@@ -50,7 +73,8 @@ const mapStateToProps = createSelector(
   (state, props) => state.wikis[makeWikiPath(props.subredditName, props.path)],
   (state, props) => state.wikiRequests[makeWikiPath(props.subredditName, props.path)],
   state => state.user,
-  (wiki, wikiRequest, user) => ({ wiki, wikiRequest, user }),
+  state => state.platform.currentPage.url,
+  (wiki, wikiRequest, user, currentPath) => ({ wiki, wikiRequest, user, currentPath }),
 );
 
 export default connect(mapStateToProps)(Wiki);
